357a3f584f84b5be3b78e985fb6917eeb35a9ed8
vdalseng <vdalseng@gmail.com>
2025-11-07 14:48:06 +0100

    Add new .NET project and update build workflow
    Build workflow:
    - Switch from Docker to Podman
    - Add build job for new .NET project
    - Add env variables for better maintainability
    
    .NET Project
    - Add new project for testing out and learning .NET
    - Includes a minimal API - uses scalar for documentation and testing
    - Includes a middleware
    - Includes Error and ExceptionHandling
    - Uses Result Pattern for the API endpoints

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,46 @@
+using MinimalAPI.Endpoints;
+using MinimalAPI.ExceptionHandlers;
+using MinimalAPI.Interfaces;
+using MinimalAPI.Middleware;
+using MinimalAPI.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services.AddExceptionHandler<GameNotFoundExceptionHandler>()
+    .AddExceptionHandler<InvalidInputExceptionHandler>()
+    .AddOpenApi()
+    .AddSingleton<IGamesService, GamesService>()
+    .AddValidation();
+
+var app = builder.Build();
+
+app.Use(
+        async (context, next) =>
+        {
+            await next.Invoke();
+        }
+    )
+    .Use(
+        async (context, next) =>
+        {
+            await next.Invoke();
+        }
+    );
+
+app.UseMiddleware<CustomMiddleware>();
+
+app.UseExceptionHandler("/Error");
+
+app.MapOpenApi();
+
+app.AddGamesEndpoints();
+
+app.MapScalarApiReference(options =>
+{
+    options.Title = "Games API";
+    options.Theme = ScalarTheme.Moon;
+});
+
+await app.RunAsync();
